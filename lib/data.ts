877f6e0a334f3e1aa21667732c657052add4c75b
Sonia Moreno <94900792+Soniaa1995@users.noreply.github.com>
2024-10-24 20:03:34 +0200

    correcciones de texto en el archivo data

--- lib/data.ts
+++ lib/data.ts
@@ -38,7 +38,7 @@ export const experiencesData = [
     title: "Bootcamp Java & Angular",
     location: "Escuela de Organización Inustrial",
     description:
-      "En este bootcamp nos enseñaron desde 0 las bases de programación con Java y realizando proyectos utilizando Spring, SpingBoot, Maven, JPA, HTML, CSS, JavaScript, Angular, Git y GitHub.",
+      "En este bootcamp, aprendimos desde cero las bases de la programación con Java y realizamos proyectos utilizando tecnologías como Spring, Spring Boot, Maven, JPA, HTML, CSS, JavaScript, Angular, Git y GitHub.",
     icon: React.createElement(LuGraduationCap),
     date: "Sep 2022 - Dec 2022 (466 horas)",
   },
@@ -46,7 +46,7 @@ export const experiencesData = [
     title: "Becaria programadora Web",
     location: "Ávoris",
     description:
-      "Las prácticas del grado las realicé en Ávoris, y pude realizar tareas de programadora web utilizando HTML, CSS, JavaScript, Git, y Jira.",
+      "Las prácticas del grado las realicé en Ávoris, y pude desarrollar tareas de programadora web utilizando HTML, CSS, JavaScript",
     icon: React.createElement(CgWorkAlt),
     date: "Abr 2024 - Jul 2024",
   },
@@ -54,7 +54,7 @@ export const experiencesData = [
     title: "Curso React",
     location: "Udemy",
     description:
-      "Este curso te muestra como hacer más de 10 proyectos desde nivel principiante hasta avanzado. Gracias a este curso he podido ampliar mis conocimientos con React y TypeScript.",
+      "Este curso te enseña a crear más de 10 proyectos desde nivel principiante hasta avanzado. Gracias a él curso he podido ampliar mis conocimientos con React y TypeScript.",
     icon: React.createElement(FaReact),
     date: "Jul 2024 - Sep 2024 (58 horas)",
   },
@@ -64,7 +64,7 @@ export const projectsData = [
   {
     title: "La vaca dice Muu",
     description:
-      "Este fue mi primer proyecto serio, el que presenté en mi Trabajo Fin de Grado de DAW. Se trata de una web simple de un restaurante.",
+      "Este fue el proyecto que presenté en mi Trabajo Fin de Grado en Desarrollo de Aplicaciones Web. Se trata de una página web sencilla de un restaurante.",
     tags: ["React", "React-Router", "Next.js", "Tailwind"],
     imageUrl: restaurante,
     url: "https://proyecto-restaurante-gc2h.vercel.app/"
@@ -72,7 +72,7 @@ export const projectsData = [
   {
     title: "Clínica Veterinaria",
     description:
-      "Es una web para registrar los pacientes de un vetinario a través de un formulario.",
+      "Es una página web diseñada para registrar a los pacientes de un veterinario a través de un formulario.",
     tags: ["React", "TypeScript", "Vite", "Zustand", "React Hook Form","Tailwind"],
     imageUrl: veterinario,
     url: "https://pacientes-veterinario-react.vercel.app/"
@@ -80,7 +80,7 @@ export const projectsData = [
   {
     title: "Cocktail",
     description:
-      "Web para consultar recetas de bebidas. Las recetas se han obtenido a través de una api.",
+      "Es una página web para consultar recetas de bebidas. Las recetas se obtienen a través de una API externa.",
     tags: ["React", "Vite", "React Router", "Zod", "Zustand", "Tailwind"],
     imageUrl: cocktail,
     url: "https://compra-bebidas-react.vercel.app/"
